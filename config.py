964dacbd46568cb05e1f4f388214993761562785
FI4A <41367817+FI4A@users.noreply.github.com>
2021-07-04 13:57:47 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+
+#BOT:
+TOKEN = "1771739004:AAEEhPuNLR4n4sppDUt0LI9Z7Hdzp6z0HAI" # ТОКЕН БОТА
+ADMIN = 1863997693# ИД АДМИНА, узнать свой айди - @get_any_telegram_id_bot
+
+DIR = "" # Путь к файлу, если нужно
+
+#USER: # https://my.telegram.org/auth
+API_ID = 1775488
+API_HASH = "693652e92b0022cb1eedbb6238fd9d36"
